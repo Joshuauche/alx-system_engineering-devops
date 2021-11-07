309ee6b9d10eb11a9059a5ea9b4e61de618fb270
joshua onwuka <contactuche02@gmail.com>
2021-11-07 12:02:08 -0800

    modified

--- 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 #  script that accepts one argument and pass it to a regular expression matching method
 # the regular expression searches for repitions
-puts ARGV[0].scan(/^\b\d{10}\b/).join
+puts ARGV[0].scan(/[A-Z]/).join
