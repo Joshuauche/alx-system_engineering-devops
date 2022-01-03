bda8b60829f3cc4ed8e0745588f566365c7078fe
joshua uche <contactuche02@gmail.com>
2022-01-03 21:16:14 +0000

    using puppet to install puppet-lint

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,6 @@
 # kill the process
 exec { 'killmenow':
+  creates => 'killmenow',
   path    => 'alx-system_engineering-devops/0x0A-configuration_management',
   creates => 'alx-system_engineering-devops/0x0A-configuration_management',
   command => 'pkill -9 -f killmenow' 
