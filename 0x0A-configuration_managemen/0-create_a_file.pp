d85c381d933e85e9a2c7f87093285ab0279b9f93
joshua uche <contactuche02@gmail.com>
2022-01-03 14:53:47 +0000

    using puppet to create a file in /tmp

--- 0x0A-configuration_managemen/0-create_a_file.pp
+++ 0x0A-configuration_managemen/0-create_a_file.pp
@@ -1,9 +1,5 @@
-# File path is /tmp/school
-# File permission is 0744
-# File owner is www-data
-# File group is www-data
-# File contains I love Puppet
-file { '/tmp/school':
+# creates file
+file { 'school':
   path    => '/tmp/school',
   mode    => '0744',
   content => 'I love Puppet',
