b6407b31fce5319b2aa63e76a119eb85b098b88d
joshua uche <contactuche02@gmail.com>
2022-01-04 09:09:38 +0000

    using puppet to terminate a process

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,5 @@
-# kill the process
+# terminate the process using pkill
 exec { 'killmenow':
-  creates => 'killmenow',
-  path    => 'alx-system_engineering-devops/0x0A-configuration_management',
-  creates => 'alx-system_engineering-devops/0x0A-configuration_management',
-  command => 'pkill -9 -f killmenow' 
+  command   => 'pkill -15 killmenow',
+  provider  => 'shell',
 }
