f89f1bf6c5267edbb9c17f01da9e3d613b01cde6
joshua uche <contactuche02@gmail.com>
2022-01-10 04:52:22 +0000

    using puppet to insstall, configure adn redirect nginx

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,14 @@
+package { 'nginx':
+    ensure  =>  'installed',
+}
+
+file_line { 'redirecting':
+    ensure => 'present',
+    path   => '/etc/nginx/sites-available/default',
+    line   => 'rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent',
+    port   =>  80,
+}
+
+file {'/var/www/html/404.html/index.html':
+    content =>  'Hello World!',
+}
