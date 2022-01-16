ac438848b562e24af9f750564fc2a12d5f610357
joshua uche <contactuche02@gmail.com>
2022-01-16 18:35:45 +0000

    creating a custom HTTP header response using puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,40 +1,37 @@
 # setting up creating a custom HTTP header response
 
-exec { 'apt-update':
-    command => '/usr/bin/apt-get update',
+
+exec { 'apt-get-update':
+  command => '/usr/bin/apt-get update',
 }
 
-# installing nginx
 package { 'nginx':
-    ensure  => installed,
-    require => Exec['apt-update'],
+  ensure  => installed,
+  require => Exec['apt-get-update'],
 }
 
 file_line { 'redirecting':
-    ensure  => 'present',
-    path    => '/etc/nginx/sites-available/default',
-    after   => 'listen 80 default_server',
-    line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-    require => Package['nginx'],
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  require => Package['nginx'],
 }
 
-# custom header response
-file_line { 'http_response':
-    ensure  => present,
-    path    => '/etc/nginx/sites-available/default',
-    after   => 'listen 80 default_server',
-    line    => 'add_header X-Served-By $hostname;',
-    require => Package['nginx'],
+file_line { 'http-response':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'add_header X-Served-By $hostname;',
+  require => Package['nginx'],
 }
 
-# write into file
-file { 'contain':
-    content => 'Holberton School',
-    require => Package['nginx'],
+file { '/var/www/html/index.html':
+  content => 'Holberton School',
+  require => Package['nginx'],
 }
 
-# restart nginx
 service { 'nginx':
-    ensure  => 'running',
-    require => Package['nginx'],
+  ensure  => running,
+  require => Package['nginx'],
 }
