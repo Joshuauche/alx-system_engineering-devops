997439b3abbc808f61b38fbe33b087daac71c9b1
joshua uche <contactuche02@gmail.com>
2022-01-16 15:47:38 +0000

    creating a custom HTTP header response using puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -12,10 +12,10 @@ file_line { 'http_response':
 }
 
 file_line { 'redirecting':
-    ensure  => 'present',
-    path    => '/etc/nginx/sites-available/default',
-    after   => 'listen 80 default_server',
-    line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+    ensure => 'present',
+    path   => '/etc/nginx/sites-available/default',
+    after  => 'listen 80 default_server',
+    line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
 file { 'content_inside':
