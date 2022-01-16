3d8cf7934b54993371917df640bbbea54c2c9b02
joshua uche <contactuche02@gmail.com>
2022-01-16 18:21:14 +0000

    creating a custom HTTP header response using puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,36 +1,36 @@
 # setting up creating a custom HTTP header response
 
-exec { 'apt_update':
-    command => 'usr/bin/apt-get update',
+exec { 'apt-get-update':
+  command => '/usr/bin/apt-get update',
 }
 
 package { 'nginx':
-    ensure  => installed,
-    require => Exec['apt_update'],
+  ensure  => installed,
+  require => Exec['apt-get-update'],
 }
 
-file_line { 'http_response':
-    ensure  => 'present',
-    path    => '/etc/nginx/sites-available/default',
-    after   => 'listen 80 default_server',
-    line    => 'add_header X-Served-By $hostname;',
-    require => Package['nginx'],
+file_line { 'a':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  require => Package['nginx'],
 }
 
-file_line { 'redirecting':
-    ensure  => 'present',
-    path    => '/etc/nginx/sites-available/default',
-    after   => 'listen 80 default_server',
-    line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-    require => Package['nginx'],
+file_line { 'b':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'add_header X-Served-By $hostname;',
+  require => Package['nginx'],
 }
 
-file { 'content_inside':
-    content => 'Holberton School',
-    require => Package['nginx'],
+file { '/var/www/html/index.html':
+  content => 'Holberton School',
+  require => Package['nginx'],
 }
 
 service { 'nginx':
-    ensure  => 'running',
-    require => Package['nginx'],
+  ensure  => running,
+  require => Package['nginx'],
 }
